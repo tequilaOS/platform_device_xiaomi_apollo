0c8023c9b6ad8cc6c8d746780440e5a55c820a38
Alexander Koskovich <akoskovich@pm.me>
2022-05-18 09:12:58 -0700

    apollo: overlay-tequila: Drop kg_security_container_min_bottom_margin
     * No longer used.
    
    Change-Id: I1a0f4bc3f5b267e3a62243f9457aabdab10e5cb7

--- device.mk
+++ device.mk
@@ -22,8 +22,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-tequila
+    $(LOCAL_PATH)/overlay
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
