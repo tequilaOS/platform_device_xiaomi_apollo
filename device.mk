87795d971d3550cf25fcf525605a3a72179c3127
LuK1337 <priv.luk@gmail.com>
2022-10-31 21:59:52 +0100

    apollo: Configure aux camera for Aperture
    Change-Id: Ib98518c7ba992e89b033f01af61c41089bd89d47

--- device.mk
+++ device.mk
@@ -23,7 +23,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-tequila
 
 # RRO overlays
 PRODUCT_PACKAGES += \
