mahajant99/device_xiaomi_apollo

apollon: Import QDCM calibration config
Change-Id: Ifc8d7c4bab926f34b9f1ce154f05b6408ed1fc27

device.mk
@@ -325,6 +325,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# QDCM
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/qdcm/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \

tequilaOS/platform_device_xiaomi_apollo

device.mk
@@ -32,6 +32,10 @@ PRODUCT_PACKAGES += \
     SettingsProviderM2007J3SG  \
     SettingsProviderM2007J3SY
 
+# QDCM
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/qdcm/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
 